d1e21b6bdcd781632fc29b474121bf400dac785e
bhawesh96 <=>
2017-10-28 22:07:17 +0530

    functions made, database integrated

--- vedanth/js/myjq.js
+++ vedanth/js/myjq.js
@@ -2,97 +2,159 @@ $(document).ready(function(){
 	window.a = '';
 	window.b = '';
 	window.c = '';
+	window.init = '';
+	getVals();
+
+function device1on() {
+	$('.device1img').attr('src','images/bulboff.png');
+	$(".device1but").val("a0");
+	$(".device1but").text("ON");
+}
+
+function device1off() {
+	$('.device1img').attr('src','images/bulbon.png');
+	$(".device1but").text("OFF");
+	$(".device1but").val("a1");
+}
+
+function device2on() {
+	$('.device2img').attr('src','images/bulboff.png');
+	$(".device2but").val("b0");
+	$(".device2but").text("ON");
+}
+
+function device2off() {
+	$('.device2img').attr('src','images/bulbon.png');
+	$(".device2but").text("OFF");
+	$(".device2but").val("b1");
+}
+
+function device3on() {
+	$('.device3img').attr('src','images/bulboff.png');
+	$(".device3but").val("c0");
+	$(".device3but").text("ON");
+}
+
+function device3off() {
+	$('.device3img').attr('src','images/bulbon.png');
+	$(".device3but").text("OFF");
+	$(".device3but").val("c1");
+}
+
+function device4on() {
+	$('.device4img').attr('src','images/bulboff.png');
+	$(".device4but").val("d0");
+	$(".device4but").text("ON");
+}
+
+function device4off() {
+	$('.device4img').attr('src','images/bulbon.png');
+	$(".device4but").text("OFF");
+	$(".device4but").val("d1");
+}
+
+function doorclose() {
+	$('.doorimg').attr('src','images/doorclose.png');
+	$(".doorbut").text("OPEN");
+	$(".doorbut").val("e0");
+}
+
+function dooropen() {
+	$('.doorimg').attr('src','images/dooropen.png');
+	$(".doorbut").text("CLOSE");
+	$(".doorbut").val("e1");
+}
+
+function fanon() {
+	$('.fanimg').attr('src','images/fanon.gif');
+	$(".fanbut").text("OFF");
+	$(".fanbut").val("f1");
+}
+
+function fanoff() {
+	$('.fanimg').attr('src','images/fanoff.png');
+	$(".fanbut").text("ON");
+	$(".fanbut").val("f0");
+	
+}
+
+	function getVals() {
+ 	$.get('php/init.php', {}, function(data){
+		console.log(data);
+		init = data.split("@")
+		console.log(init);
+	 	if(init[0] == 'a1') device1off();
+	 	if(init[1] == 'b1')	device2off();
+	 	if(init[2] == 'c1') device3off();
+	 	if(init[3] == 'd1') device4off();
+	 	if(init[4] == 'e1') dooropen();
+	 	if(init[5] == 'f1') fanon();
+	 	$(".tempdata").text(init[6]);
+	 	$(".moisturedata").text(init[7]);
+	 	if(init[8]=='0')
+	 		$(".soildata").text("Dry");
+	 	else if(init[8]=='1')
+		 	$(".soildata").text("Fine");
+	 	else if(init[8]=='2')
+	 		$(".soildata").text("Too wet");
+	});
+ }
+
 			
 		$('.device1but').click(function(){
  			if($('.device1img').attr('src')=='images/bulbon.png')
 			{
-				$('.device1img').attr('src','images/bulboff.png');
-				$(".device1but").val("a0");
-				$(".device1but").text("ON");
-				//a ='a0';
-				
+				device1on();	
 			}
 		else if($('.device1img').attr('src')=='images/bulboff.png')
 			{
-			 	$('.device1img').attr('src','images/bulbon.png');
-			 	$(".device1but").text("OFF");
-			 	$(".device1but").val("a1");
-			 	//a = 'a1';
-				
+			 	device1off();
 			}
 	});
 
 		$('.device2but').click(function(){
  			if($('.device2img').attr('src')=='images/bulbon.png')
 			{
-				$('.device2img').attr('src','images/bulboff.png');
-				$(".device2but").val("b0");
-				$(".device2but").text("ON");
-				//b = 'b0';
+				device2on();
 				
 			}
 		else if($('.device2img').attr('src')=='images/bulboff.png')
 			{
-			 	$('.device2img').attr('src','images/bulbon.png');
-			 	$(".device2but").val("b1");
-			 	$(".device2but").text("OFF");
-			 	//b = 'b1';
-			 	
+			 	device2off();
 			}
 	});
 
 		$('.device3but').click(function(){
  			if($('.device3img').attr('src')=='images/bulbon.png')
 			{
-				$('.device3img').attr('src','images/bulboff.png');
-				$(".device3but").val("c0");
-				$(".device3but").text("ON");
-				//b = 'b0';
-				
+				device3on();
 			}
 		else if($('.device3img').attr('src')=='images/bulboff.png')
 			{
-			 	$('.device3img').attr('src','images/bulbon.png');
-			 	$(".device3but").val("c1");
-			 	$(".device3but").text("OFF");
-			 	//b = 'b1';
-			 	
+			 	device3off();
 			}
 	});
 
 		$('.device4but').click(function(){
  			if($('.device4img').attr('src')=='images/bulbon.png')
 			{
-				$('.device4img').attr('src','images/bulboff.png');
-				$(".device4but").val("d0");
-				$(".device4but").text("ON");
-				//b = 'b0';
-				
+				device4on();
 			}
 		else if($('.device4img').attr('src')=='images/bulboff.png')
 			{
-			 	$('.device4img').attr('src','images/bulbon.png');
-			 	$(".device4but").val("d1");
-			 	$(".device4but").text("OFF");
-			 	//b = 'b1';
-			 	
+			 	device4off();
 			}
 	});
 
 		$('.doorbut').click(function(){
  			if($('.doorimg').attr('src')=='images/dooropen.png')
 			{
-				$('.doorimg').attr('src','images/doorclose.png');
-				$(".doorbut").text("OPEN");
-				$(".doorbut").val("e0");
+				doorclose();
 				
 			}
 		else if($('.doorimg').attr('src')=='images/doorclose.png')
 			{
-			 	$('.doorimg').attr('src','images/dooropen.png');
-			 	$(".doorbut").text("CLOSE");
-			 	$(".doorbut").val("e1");
-				
+			 	dooropen();
 			}
 	});
 
@@ -100,62 +162,41 @@ $(document).ready(function(){
 $('.fanbut').click(function(){
  			if($('.fanimg').attr('src')=='images/fanoff.png')
 			{
-				$('.fanimg').attr('src','images/fanon.gif');
-				$(".fanbut").text("OFF");
-				$(".fanbut").val("f1");
-				
+				fanon();
 			}
 		else if($('.fanimg').attr('src')=='images/fanon.gif')
 			{
-			 	$('.fanimg').attr('src','images/fanoff.png');
-			 	$(".fanbut").text("ON");
-			 	$(".fanbut").val("f0");
-			 	
+			 	fanoff();
 			}
 	});
 
 $('.tempbut').click(function(){
- 			jQuery.get('php/temp.txt', function(data) {
- 				$(".tempdata").text(data);
-			});
+ 		// 	jQuery.get('php/temp.txt', function(data) {
+ 		// 		$(".tempdata").text(data);
+			// });
+			getVals();
 	});
 
 $('.moisturebut').click(function(){
- 			jQuery.get('php/moisture.txt', function(data) {
- 				$(".moisturedata").text(data);
-			});
+ 		// 	jQuery.get('php/moisture.txt', function(data) {
+ 		// 		$(".moisturedata").text(data);
+			// });
+			getVals();
 	});
 $('.soilbut').click(function(){
- 			jQuery.get('php/soil.txt', function(data) {
- 				if(data='0')
- 				$(".soildata").text("Dry");
- 				else if(data='1')
- 				$(".soildata").text("Fine");
- 				else if(data='2')
- 				$(".soildata").text("Too wet");
-			});
+ 		// 	jQuery.get('php/soil.txt', function(data) {
+ 		// 		if(data='0')
+ 		// 		$(".soildata").text("Dry");
+ 		// 		else if(data='1')
+ 		// 		$(".soildata").text("Fine");
+ 		// 		else if(data='2')
+ 		// 		$(".soildata").text("Too wet");
+			// });
+			getVals();
 	});
 
 $('.reset').click(function(){
-		$(".device1but").val("a0");
-		$('.device1img').attr('src','images/bulboff.png');
-		$(".device1but").text("ON");
-		$(".device2but").val("b0");
-		$('.device2img').attr('src','images/bulboff.png');
-		$(".device2but").text("ON");
-		$(".device3but").val("c0");
-		$('.device3img').attr('src','images/bulboff.png');
-		$(".device3but").text("ON");
-		$(".device4utt").val("d0");
-		$('.device4img').attr('src','images/bulboff.png');
-		$(".device4but").text("ON");
-		$(".doorbut").val("e0");
-		$('.doorimg').attr('src','images/doorclose.png');
-		$(".doorbut").text("OPEN");
-		$(".fanbut").val("f0"); 		
-		$('.fanimg').attr('src','images/fanoff.png');
-		$(".fanbut").text("ON");
- 			
+		device1on();device2on();device3on();device4on();doorclose();fanoff();	
 	});
 
 
@@ -169,16 +210,16 @@ $(document).click(function() {
 		var door = $(".doorbut").val();
 		var fan = $(".fanbut").val();
 		$('.data2').text(r1light1+r2light2+halllight1+halllight2+door+fan);
-  	 	$.get('php/room1.php', { light1:r1light1}, function(data){
-
+		$.get('php/room1.php', { light1:r1light1}, function(data){
+				console.log(data);
 			 	});
 
 		$.get('php/room2.php', { light1:r2light2}, function(data){
-
+				console.log(data);
 			 	});
 
   	 	$.get('php/hall.php', { light1:halllight1, light2:halllight2, fan : fan, door : door}, function(data){
-
+  	 			console.log(data);
 			 	});
 
 		
--- vedanth/php/room1.php
+++ vedanth/php/room1.php
@@ -4,8 +4,18 @@ $msg = $_GET['light1'];
 //$string = $msg." ".$msg1." ".$msg2." ".$msg3;
 $string = $msg[1];
 echo $msg;
-$myfile = fopen("room1.txt", "w") or die("Unable to open file!");
+$myfile = fopen("room1.txt", "w") or die("Unable to open file Room1.txt!");
 fwrite($myfile, $string);
 fclose($myfile);
-include "main.php"
+include "main.php";
+include "db.php";
+
+$sql = "UPDATE status SET device1='".$msg."'";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
 ?>
